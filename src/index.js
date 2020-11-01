d0782d74a626158333420d32f16f2e8374456e66
Sushant <prajwal.pandit7@gmail.com>
2020-11-01 17:54:01 +0530

    Connected to websocket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+
+const socketio = require('socket.io');
+
 const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log(`Server is on port ${port}!`);
+io.on('connection', () => {
+    console.log("New websocket connetction");
 });
+
+server.listen(port, () => {
+    console.log(`Server is on port ${port}!`);
+}); 
